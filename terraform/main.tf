70fd39543ec144f6fbb01db1117df5abf81296c5
NstecEmp-014 <nstecemp_014@fullness.email>
2025-09-09 12:40:00 +0900

    環境整理2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,31 @@
-provider "aws" { # AWSプロバイダーの設定開始
-  region = var.aws_region # 使用するAWSリージョンを変数から指定
-} # AWSプロバイダーの設定終了
+# ---------------------------------------------
+# Terraform configuration //
+# ---------------------------------------------
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
-resource "aws_instance" "web" { # EC2インスタンス(web)のリソース定義開始
-  ami           = var.ami_id # 使用するAMI IDを変数から指定
-  instance_type = "t2.micro" # インスタンスタイプを指定
-  tags = { # タグの設定開始
-    Name = "self-directed-training-web" # インスタンス名タグ
-  } # タグの設定終了
-} # EC2インスタンス(web)のリソース定義終了
+# ---------------------------------------------
+# Provider
+# ---------------------------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
 
-resource "aws_db_instance" "db" { # RDSデータベースインスタンス(db)のリソース定義開始
-  allocated_storage    = 20 # データベースのストレージ容量(GB)
-  engine               = "mysql" # 使用するDBエンジン
-  instance_class       = "db.t3.micro" # DBインスタンスタイプ
-  db_name              = "stockdb" # DB名
-  username             = var.db_username # DBユーザー名(変数から指定)
-  password             = var.db_password # DBパスワード(変数から指定)
-  parameter_group_name = "default.mysql8.0" # パラメータグループ名
-  db_subnet_group_name = var.db_subnet_group_name # DBサブネットグループ名(変数から指定)
-  vpc_security_group_ids = [var.db_security_group_id] # セキュリティグループ(必要なら)
-} # RDSデータベースインスタンス(db)のリソース定義終了
+# ---------------------------------------------
+# Variables
+# ---------------------------------------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
