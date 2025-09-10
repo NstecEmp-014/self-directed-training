7f158b06e513001ed96145599213d7575e6a2e64
NstecEmp-014 <nstecemp_014@fullness.email>
2025-09-10 14:02:48 +0900

    DB修正

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,9 +1,8 @@
 -- 株テーブルのみ
-drop table if exists stock;
 create table stock(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,     -- 銘柄名
-  code VARCHAR(10) NOT NULL,     -- 証券コード
-  price INTEGER NOT NULL,        -- 現在価格
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  code VARCHAR(10) NOT NULL,
+  price INTEGER NOT NULL,
   delete_flag INTEGER NOT NULL DEFAULT 0
 );
