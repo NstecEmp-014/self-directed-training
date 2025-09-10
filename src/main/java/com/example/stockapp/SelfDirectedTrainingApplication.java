5e4b03616b926810e000e0087efe10449e96fa20
NstecEmp-014 <nstecemp_014@fullness.email>
2025-09-10 15:24:44 +0900

    test2

--- src/main/java/com/example/stockapp/SelfDirectedTrainingApplication.java
+++ src/main/java/com/example/stockapp/SelfDirectedTrainingApplication.java
@@ -1,8 +1,10 @@
 package com.example.stockapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.example.stockapp.repository")
 @SpringBootApplication
 public class SelfDirectedTrainingApplication {
     public static void main(String[] args) {
--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -41,6 +41,16 @@ resource "aws_security_group_rule" "web_out_tcp3000" {
   source_security_group_id = aws_security_group.app_sg.id
 }
 
+# 8080/TCPインバウンドルール追加
+resource "aws_security_group_rule" "web_in_8080" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 8080
+  to_port           = 8080
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 # app security group
 resource "aws_security_group" "app_sg" {
   name        = "${var.project}-${var.environment}-app-sg"
