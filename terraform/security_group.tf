0dafc826a9f1b7aecb28d447144123a3426b738b
NstecEmp-014 <nstecemp_014@fullness.email>
2025-09-22 17:48:49 +0900

    AWSの環境構築２

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -173,7 +173,8 @@ resource "aws_security_group" "db_sg" {
   }
 }
 
-resource "aws_security_group_rule" "db_in_tcp3306" {
+# PostgreSQL用インバウンドルール（5432）
+resource "aws_security_group_rule" "db_in_tcp5432" {
   security_group_id        = aws_security_group.db_sg.id
   type                     = "ingress"
   protocol                 = "tcp"
